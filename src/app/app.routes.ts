7f212fbc6d2c3fdc6c2645e5cc84a36a6a16af84
unknown <gabrielagd04@gmail.com>
2024-10-28 17:43:05 -0400

    Dashboard diseno completo + finalizacion diseno foro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LogInComponent } from './views/log-in/log-in.component';
 import { ForoListComponent } from './views/foro-list/foro-list.component';
 import { ForoDetailComponent } from './views/foro-detail/foro-detail.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { NewsComponent } from './views/news/news.component';
 
 export const routes: Routes = [
     { path: 'login', component: LogInComponent },
@@ -10,5 +11,6 @@ export const routes: Routes = [
     { path: 'foro', component: ForoListComponent },
     { path: 'foro/:id', component: ForoDetailComponent },
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'informaciones', component: NewsComponent}
 
   ];
