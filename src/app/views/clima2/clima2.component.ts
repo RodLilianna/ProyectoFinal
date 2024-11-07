37e8a2f7e503aa2de1420684230ad3e03d3d3793
Lilianna Rodriguez <liliannarguez1@gmail.com>
2024-11-07 02:28:01 -0500

    Todas las vistas listas

---
+++ src/app/views/clima2/clima2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clima2',
+  standalone: true,
+  imports: [],
+  templateUrl: './clima2.component.html',
+  styleUrl: './clima2.component.css'
+})
+export class Clima2Component {
+
+}
